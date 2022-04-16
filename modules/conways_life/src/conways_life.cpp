68ec27bcb27ed131eb19919506730b9401802447
EvgenyPinezhanin <pinezhanin@yandex.ru>
2022-04-17 00:18:54 +0300

    add tests

--- modules/conways_life/src/conways_life.cpp
+++ modules/conways_life/src/conways_life.cpp
@@ -5,3 +5,11 @@
 #include <vector>
 
 ConwaysLife::ConwaysLife() : grid(new std::vector<std::vector<bool>>) {}
+
+ConwaysLife::ConwaysLife(const ConwaysLife &cl) {}
+
+void ConwaysLife::setGrid(const std::vector<std::vector<bool>> &_grid) {}
+
+std::vector<std::vector<bool>> ConwaysLife::getGrid() { return std::vector<std::vector<bool>>(); }
+
+void newGen() {}
--- modules/conways_life/test/test_conways_life.cpp
+++ modules/conways_life/test/test_conways_life.cpp
@@ -8,6 +8,25 @@ TEST(conways_life, default_constructor) {
     ASSERT_NO_THROW(ConwaysLife life);
 }
 
-// TEST(coways_life, default_grid) {
-//     ASSERT
-// }
+TEST(coways_life, get_default_grid) {
+    ConwaysLife life;
+    std::vector<std::vector<bool>> def_grid{ {0, 0, 0, 0, 0},
+                                             {0, 0, 0, 0, 0},
+                                             {0, 0, 0, 0, 0},
+                                             {0, 0, 0, 0, 0},
+                                             {0, 0, 0, 0, 0} };
+    std::vector<std::vector<bool>> grid = life.getGrid(); 
+    ASSERT_EQ(def_grid, grid);
+}
+
+TEST(conways_life, can_set_grid_and_get_grid) {
+    ConwaysLife life;
+    std::vector<std::vector<bool>> grid{ {0, 1, 0, 1, 0},
+                                         {0, 0, 1, 0, 0},
+                                         {0, 0, 0, 0, 1},
+                                         {0, 1, 0, 0, 0},
+                                         {0, 0, 0, 1, 0} };
+    life.setGrid(grid);
+    std::vector<std::vector<bool>> life_grid = life.getGrid();
+    ASSERT_EQ(grid, life_grid);
+}
