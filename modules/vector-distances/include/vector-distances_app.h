943283cecddf08b6d96f7c3fee1c8e3b08f34f79
EvgenyPinezhanin <pinezhanin@yandex.ru>
2022-05-06 21:36:57 +0300

    add appliaction

--- modules/vector-distances/include/vector-distances_app.h
+++ modules/vector-distances/include/vector-distances_app.h
@@ -4,8 +4,20 @@
 #define MODULES_VECTOR_DISTANCES_INCLUDE_VECTOR_DISTANCES_APP_H_
 
 #include <string>
+#include <vector>
 
 class Application {
+ private:
+    std::string message;
+    typedef struct {
+        std::vector<float> vec1;
+        std::vector<float> vec2;
+        int metric;
+    } Arguments;
+
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+
  public:
     Application();
     std::string operator()(int argc, const char** argv);
